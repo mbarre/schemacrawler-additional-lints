71b7afa523d1790584232ce4ea54bf9f03e86c5d
mbarre <michele.barre@gmail.com>
2015-07-24 10:29:46 +1100

    test unit on LinterTableNameNotInLowerCase class

--- src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterTableNameNotInLowerCaseTest.java
+++ src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterTableNameNotInLowerCaseTest.java
@@ -4,12 +4,17 @@
 package io.github.mbarre.schemacrawler.test.tool.linter;
 
 import io.github.mbarre.schemacrawler.test.utils.PostgreSqlDatabase;
+import io.github.mbarre.schemacrawler.tool.linter.LinterTableNameNotInLowerCase;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.tools.lint.Linter;
+import schemacrawler.tools.lint.LinterRegistry;
 
 /**
  * @author mbarre
@@ -26,9 +31,13 @@ public class LinterTableNameNotInLowerCaseTest {
 	}
 	
 	@Test
-	public void testLint(){
-		logger.info("test!");
-		Assert.assertTrue(true);
+	public void testLint() throws SchemaCrawlerException{
+		
+		final LinterRegistry registry = new LinterRegistry();
+		Linter linter = registry.lookupLinter(LinterTableNameNotInLowerCase.class.getName());
+		
+		Assert.assertNotNull(linter);
+
 	}
 
 }
