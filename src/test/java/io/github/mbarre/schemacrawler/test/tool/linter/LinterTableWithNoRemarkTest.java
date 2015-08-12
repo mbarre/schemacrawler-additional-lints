3862c02d77c0c0af4dea1a1eddf5141de97d5e3d
adriens <Adrien.Sales@gmail.com>
2015-08-13 08:33:29 +1100

    Removed unused import

--- src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterTableWithNoRemarkTest.java
+++ src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterTableWithNoRemarkTest.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
-import schemacrawler.schemacrawler.SchemaInfoLevel;
 import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
 import schemacrawler.tools.executable.Executable;
 import schemacrawler.tools.executable.SchemaCrawlerExecutable;
