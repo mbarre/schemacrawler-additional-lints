87b860a4408ee747acec6633aeafe0c8d81eceec
adriens <Adrien.Sales@gmail.com>
2015-08-14 08:19:53 +1100

    Added sample example for postgres.

--- src/main/java/io/github/mbarre/schemacrawler/tool/linter/LinterColumnContentNotNormalized.java
+++ src/main/java/io/github/mbarre/schemacrawler/tool/linter/LinterColumnContentNotNormalized.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+TODO : implement sample option
+
+SELECT * FROM myTable
+WHERE attribute = 'myValue'
+ORDER BY random()
+LIMIT 1000;
  */
 package io.github.mbarre.schemacrawler.tool.linter;
 
