7f64c45c857ac32a3f9cbac94bd08369361904c3
mbarre <michele.barre@gmail.com>
2016-02-09 09:12:27 +1100

    Lint jsonContent fails if postgres < 9.4 #38

--- src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterJsonContentTest.java
+++ src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterJsonContentTest.java
@@ -1,6 +1,6 @@
-/**
- * 
- */
+ /**
+  *
+  */
 package io.github.mbarre.schemacrawler.test.tool.linter;
 
 import io.github.mbarre.schemacrawler.test.utils.LintWrapper;
@@ -17,6 +17,7 @@ import schemacrawler.tools.lint.LinterRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 
@@ -26,40 +27,47 @@ import java.util.List;
  * @since 1.0.1
  */
 public class LinterJsonContentTest extends BaseLintTest {
-
-	private static final String CHANGE_LOG_JSON_CHECK = "src/test/db/liquibase/jsonContentCheck/db.changelog.xml";
-	private Logger LOGGER = LoggerFactory.getLogger(LinterJsonContentTest.class);
-	private static PostgreSqlDatabase database;
-
-	@BeforeClass
-	public static void  init(){
-		database = new PostgreSqlDatabase();
-		database.setUp(CHANGE_LOG_JSON_CHECK);
-	}
-
-	@Test
-	public void testLint() throws Exception{
-
-		final LinterRegistry registry = new LinterRegistry();
-		Assert.assertTrue(registry.hasLinter(LinterJsonContent.class.getName()));
-
-		final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
-		// Set what details are required in the schema - this affects the
-		// time taken to crawl the schema
-		options.setSchemaInfoLevel(SchemaInfoLevelBuilder.standard());
-		options.setTableNamePattern("test_json");
-		
-		Connection connection = DriverManager.getConnection(PostgreSqlDatabase.CONNECTION_STRING, 
-				PostgreSqlDatabase.USER_NAME, database.getPostgresPassword());
-		
-		List<LintWrapper> lints = executeToJsonAndConvertToLintList(options, connection);
-		
-		Assert.assertEquals(1, lints.size());
-		Assert.assertEquals(LinterJsonContent.class.getName(), lints.get(0).getId());
-		Assert.assertEquals("public.test_json.content", lints.get(0).getValue());
-		Assert.assertEquals("should be JSON or JSONB type.", lints.get(0).getDescription());
-		Assert.assertEquals("high", lints.get(0).getSeverity());
-
-	}
-
+    
+    private static final String CHANGE_LOG_JSON_CHECK = "src/test/db/liquibase/jsonContentCheck/db.changelog.xml";
+    private Logger LOGGER = LoggerFactory.getLogger(LinterJsonContentTest.class);
+    private static PostgreSqlDatabase database;
+    private static boolean jsonbSupport = false;
+    
+    @BeforeClass
+    public static void  init() throws SQLException{
+        database = new PostgreSqlDatabase();
+        
+        if("9.4".compareTo(database.getDbVersion()) <= 0){
+            jsonbSupport = true;
+            database.setUp(CHANGE_LOG_JSON_CHECK);
+        }
+    }
+    
+    @Test
+    public void testLint() throws Exception{
+        
+        if(jsonbSupport){
+            
+            final LinterRegistry registry = new LinterRegistry();
+            Assert.assertTrue(registry.hasLinter(LinterJsonContent.class.getName()));
+            
+            final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
+            // Set what details are required in the schema - this affects the
+            // time taken to crawl the schema
+            options.setSchemaInfoLevel(SchemaInfoLevelBuilder.standard());
+            options.setTableNamePattern("test_json");
+            
+            Connection connection = DriverManager.getConnection(PostgreSqlDatabase.CONNECTION_STRING,
+                    PostgreSqlDatabase.USER_NAME, database.getPostgresPassword());
+            
+            List<LintWrapper> lints = executeToJsonAndConvertToLintList(options, connection);
+            
+            Assert.assertEquals(1, lints.size());
+            Assert.assertEquals(LinterJsonContent.class.getName(), lints.get(0).getId());
+            Assert.assertEquals("public.test_json.content", lints.get(0).getValue());
+            Assert.assertEquals("should be JSON or JSONB type.", lints.get(0).getDescription());
+            Assert.assertEquals("high", lints.get(0).getSeverity());
+        }
+    }
+    
 }
--- src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterJsonTypeColumnTest.java
+++ src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterJsonTypeColumnTest.java
@@ -1,6 +1,6 @@
-/**
- *
- */
+ /**
+  *
+  */
 package io.github.mbarre.schemacrawler.test.tool.linter;
 
 import io.github.mbarre.schemacrawler.test.utils.LintWrapper;
@@ -25,40 +25,48 @@ import java.util.List;
  * @author mbarre
  */
 public class LinterJsonTypeColumnTest extends BaseLintTest {
-
+    
     private static final String CHANGE_LOG_JSON_CHECK = "src/test/db/liquibase/jsonbCheck/db.changelog.xml";
     private Logger LOGGER = LoggerFactory.getLogger(LinterJsonTypeColumnTest.class);
     private static PostgreSqlDatabase database;
-
+    private static boolean jsonbSupport = false;
+    
+    
     @BeforeClass
     public static void  init(){
         database = new PostgreSqlDatabase();
-        database.setUp(CHANGE_LOG_JSON_CHECK);
+        if("9.4".compareTo(database.getDbVersion()) <= 0){
+            jsonbSupport = true;
+            database.setUp(CHANGE_LOG_JSON_CHECK);
+        }
     }
-
+    
     @Test
     public void testLint() throws Exception{
-
-        final LinterRegistry registry = new LinterRegistry();
-        Assert.assertTrue(registry.hasLinter(LinterJsonTypeColumn.class.getName()));
-
-        final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
-        // Set what details are required in the schema - this affects the
-        // time taken to crawl the schema
-        options.setSchemaInfoLevel(SchemaInfoLevelBuilder.standard());
-        options.setTableNamePattern("test_json");
-
-        Connection connection = DriverManager.getConnection(PostgreSqlDatabase.CONNECTION_STRING,
-                PostgreSqlDatabase.USER_NAME, database.getPostgresPassword());
-
-        List<LintWrapper> lints = executeToJsonAndConvertToLintList(options, connection);
-
-        Assert.assertEquals(1,lints.size());
-        Assert.assertEquals(LinterJsonTypeColumn.class.getName(), lints.get(0).getId());
-        Assert.assertEquals("content_json", lints.get(0).getValue());
-        Assert.assertEquals("\"jsonb\" type should be used instead of \"json\" to store JSON data.", lints.get(0).getDescription());
-        Assert.assertEquals("high", lints.get(0).getSeverity());
+        
+        if(jsonbSupport){
+            
+            final LinterRegistry registry = new LinterRegistry();
+            Assert.assertTrue(registry.hasLinter(LinterJsonTypeColumn.class.getName()));
+            
+            final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
+            // Set what details are required in the schema - this affects the
+            // time taken to crawl the schema
+            options.setSchemaInfoLevel(SchemaInfoLevelBuilder.standard());
+            options.setTableNamePattern("test_json");
+            
+            Connection connection = DriverManager.getConnection(PostgreSqlDatabase.CONNECTION_STRING,
+                    PostgreSqlDatabase.USER_NAME, database.getPostgresPassword());
+            
+            List<LintWrapper> lints = executeToJsonAndConvertToLintList(options, connection);
+            
+            Assert.assertEquals(1,lints.size());
+            Assert.assertEquals(LinterJsonTypeColumn.class.getName(), lints.get(0).getId());
+            Assert.assertEquals("content_json", lints.get(0).getValue());
+            Assert.assertEquals("\"jsonb\" type should be used instead of \"json\" to store JSON data.", lints.get(0).getDescription());
+            Assert.assertEquals("high", lints.get(0).getSeverity());
+        }
     }
-
-
+    
+    
 }
