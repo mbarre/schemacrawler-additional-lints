36d9a33ba0c499794de65361175faddf40a46725
mbarre <michele.barre@gmail.com>
2016-02-19 13:57:54 +1100

    Add a Lint for BLOB #1

---
+++ src/main/java/io/github/mbarre/schemacrawler/tool/linter/LinterBlobTypeColumn.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.mbarre.schemacrawler.tool.linter;
+
+import java.sql.Connection;
+import java.sql.Types;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import schemacrawler.schema.Column;
+import schemacrawler.schema.Table;
+import schemacrawler.tools.lint.BaseLinter;
+import schemacrawler.tools.lint.LintSeverity;
+
+/**
+ *
+ * @author barmi83
+ */
+public class LinterBlobTypeColumn extends BaseLinter {
+    private static final Logger LOGGER = Logger.getLogger(LinterBlobTypeColumn.class.getName());
+    
+    
+    
+    /**
+     * The lint that parses and test primary keys
+     *
+     */
+    public LinterBlobTypeColumn() {
+        super();
+        setSeverity(LintSeverity.high);
+    }
+    
+    /**
+     * Get lint descrption
+     * @return lint description
+     */
+    @Override
+    public String getDescription() {
+        return getSummary();
+    }
+    
+    /**
+     * Get lint Summary
+     * @return lint Summary
+     */
+    @Override
+    public String getSummary() {
+        return "BLOB should not be used.";
+    }
+    
+    /**
+     * The lint that does the job
+     * @param table table
+     * @param connection connection
+     */
+    @Override
+    protected void lint(final Table table, final Connection connection) {
+        
+   
+        for (Column column : table.getColumns()) {
+                if(column.getColumnDataType().getJavaSqlType().getJavaSqlType() == Types.BINARY){
+                    addLint(table, getDescription(), column.getName());
+                    LOGGER.log(Level.INFO, "{0} is BLOB type.", column.getFullName());
+                }
+            }
+    }
+    
+}
---
+++ src/test/java/io/github/mbarre/schemacrawler/test/tool/linter/LinterBlobTypeColumnTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.mbarre.schemacrawler.test.tool.linter;
+
+import io.github.mbarre.schemacrawler.test.utils.LintWrapper;
+import io.github.mbarre.schemacrawler.test.utils.PostgreSqlDatabase;
+import io.github.mbarre.schemacrawler.tool.linter.LinterBlobTypeColumn;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.tools.lint.LinterRegistry;
+
+/**
+ *
+ * @author barmi83
+ */
+public class LinterBlobTypeColumnTest extends BaseLintTest {
+    
+    private static final String CHANGE_LOG_BLOB_CHECK = "src/test/db/liquibase/blobCheck/db.changelog.xml";
+    private static final Logger LOGGER = LoggerFactory.getLogger(LinterBlobTypeColumnTest.class);
+    private static PostgreSqlDatabase database;
+    
+    @BeforeClass
+    public static void  init(){
+        database = new PostgreSqlDatabase();
+        database.setUp(CHANGE_LOG_BLOB_CHECK);
+    }
+    
+    @Test
+    public void testLint() throws Exception{
+            
+            final LinterRegistry registry = new LinterRegistry();
+            Assert.assertTrue(registry.hasLinter(LinterBlobTypeColumn.class.getName()));
+            
+            final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
+            // Set what details are required in the schema - this affects the
+            // time taken to crawl the schema
+            options.setSchemaInfoLevel(SchemaInfoLevelBuilder.standard());
+            options.setTableNamePattern("test_blob");
+            
+            Connection connection = DriverManager.getConnection(PostgreSqlDatabase.CONNECTION_STRING,
+                    PostgreSqlDatabase.USER_NAME, database.getPostgresPassword());
+            
+            List<LintWrapper> lints = executeToJsonAndConvertToLintList(options, connection);
+            
+            Assert.assertEquals(1,lints.size());
+            Assert.assertEquals(LinterBlobTypeColumn.class.getName(), lints.get(0).getId());
+            Assert.assertEquals("content_blob", lints.get(0).getValue());
+            Assert.assertEquals("BLOB should not be used.", lints.get(0).getDescription());
+            Assert.assertEquals("high", lints.get(0).getSeverity());
+        }
+}
