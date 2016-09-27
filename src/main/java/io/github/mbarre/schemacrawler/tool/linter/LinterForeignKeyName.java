e61ab36fde1d3c7063d53ba2f0bcb1b0fb00d5c2
mbarre <michele.barre@gmail.com>
2016-09-27 17:15:43 +1100

    Foreign key name, fix #62

--- src/main/java/io/github/mbarre/schemacrawler/tool/linter/LinterForeignKeyName.java
+++ src/main/java/io/github/mbarre/schemacrawler/tool/linter/LinterForeignKeyName.java
@@ -1,7 +1,88 @@
 package io.github.mbarre.schemacrawler.tool.linter;
 
+import schemacrawler.schema.*;
+import schemacrawler.schemacrawler.Config;
+import schemacrawler.schemacrawler.SchemaCrawlerException;
+import schemacrawler.tools.lint.BaseLinter;
+import schemacrawler.tools.lint.LintSeverity;
+
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.util.Objects.requireNonNull;
+
 /**
+ *  Check that foreign key name has right prefix or suffix name
  * Created by barmi83 on 12/09/16.
  */
-public class LinterForeignKeyName {
+
+public class LinterForeignKeyName  extends BaseLinter {
+
+    private static final Logger LOGGER = Logger.getLogger(LinterForeignKeyName.class.getName());
+
+    public static final String PREFIX_CONFIG_PARAM = "foreignKeyPrefix";
+    public static final String SUFFIX_CONFIG_PARAM = "foreignKeySuffix";
+
+    private String prefix;
+    private String suffix;
+
+    public LinterForeignKeyName() {
+        setSeverity(LintSeverity.medium);
+    }
+
+    @Override
+    public String getSummary() {
+        return "FK name does not contain required prefix or suffix.";
+    }
+
+    /**
+     * Get the description
+     * @return the description
+     */
+    @Override
+    public String getDescription() {
+        return "Foreign key data type does not match Primary key";
+    }
+
+    @Override
+    public void configure(Config config) {
+        prefix = config.getStringValue(PREFIX_CONFIG_PARAM, "");
+        suffix = config.getStringValue(SUFFIX_CONFIG_PARAM, "");
+    }
+
+    @Override
+    protected void lint(Table table, Connection connection) throws SchemaCrawlerException {
+
+        LOGGER.log(Level.CONFIG, "<"+PREFIX_CONFIG_PARAM+"> parameter set to {0}%", prefix);
+        LOGGER.log(Level.CONFIG, "<"+SUFFIX_CONFIG_PARAM+"> parameter set to {0}%", suffix);
+
+        requireNonNull(table, "No table provided");
+        findMismatchedForeignKeys(table);
+
+    }
+
+    private void findMismatchedForeignKeys(final Table table){
+
+        if (table != null && !(table instanceof View))
+        {
+            for (final ForeignKey foreignKey: table.getImportedForeignKeys())
+            {
+                for (final ForeignKeyColumnReference columnReference: foreignKey)
+                {
+                    final Column fkColumn = columnReference.getForeignKeyColumn();
+                    if (!fkColumn.getName().startsWith(prefix))
+                    {
+                        addTableLint(table, "Foreign key does not start with prefix \"" + prefix +"\"", foreignKey);
+
+                    }
+                    if (!fkColumn.getName().endsWith(suffix))
+                    {
+                        addTableLint(table, "Foreign key does not ends with suffix \"" + suffix +"\"", foreignKey);
+                    }
+                }
+            }
+        }
+    }
+
 }
---
+++ src/test/java/io/github/mbarre/schemacrawler/tool/linter/LinterForeignKeyNameTest.java
@@ -0,0 +1,82 @@
+package io.github.mbarre.schemacrawler.tool.linter;
+
+/*
+ * #%L
+ * Additional SchemaCrawler Lints
+ * %%
+ * Copyright (C) 2015 - 2016 github
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import io.github.mbarre.schemacrawler.test.utils.LintWrapper;
+import io.github.mbarre.schemacrawler.test.utils.PostgreSqlDatabase;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
+import schemacrawler.tools.lint.LinterRegistry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+
+/**
+ * Check foreign key name
+ * @author mbarre
+ * @since 1.1.10
+ */
+public class LinterForeignKeyNameTest extends BaseLintTest {
+    
+    private static final String CHANGE_LOG_FK_NAME_CHECK = "src/test/db/liquibase/foreignKeyNameCheck/db.changelog.xml";
+    private static PostgreSqlDatabase database;
+    
+    @BeforeClass
+    public static void  init() throws SQLException{
+        database = new PostgreSqlDatabase();
+        database.setUp(CHANGE_LOG_FK_NAME_CHECK);
+    }
+    
+    @Test
+    public void testLint() throws Exception{
+        
+        final LinterRegistry registry = new LinterRegistry();
+        Assert.assertTrue(registry.hasLinter(LinterForeignKeyMismatchLazy.class.getName()));
+        
+        final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
+        options.setSchemaInfoLevel(SchemaInfoLevelBuilder.standard());
+        
+        Connection connection = DriverManager.getConnection(PostgreSqlDatabase.CONNECTION_STRING,
+                PostgreSqlDatabase.USER_NAME, database.getPostgresPassword());
+        
+        List<LintWrapper> lints = executeToJsonAndConvertToLintList(options, connection);
+        Assert.assertEquals(1,lints.size());
+        Assert.assertEquals(LinterForeignKeyName.class.getName(), lints.get(0).getId());
+        Assert.assertEquals("fk_table3", lints.get(0).getValue());
+        Assert.assertEquals("Foreign key does not ends with suffix \"_id\"", lints.get(0).getDescription());
+        Assert.assertEquals("medium", lints.get(0).getSeverity());
+        
+       
+    }
+    
+   
+    
+   
+    
+}
