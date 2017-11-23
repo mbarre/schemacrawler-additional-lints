e247df115a1fb20205a798ec0e15ae096b08e2d9
mbarre <michele.barre@ville-noumea.nc>
2017-11-24 09:35:59 +1100

    fix #93, test sur un sample dont la taille est parametrable

--- src/main/java/io/github/mbarre/schemacrawler/tool/linter/LinterXmlContent.java
+++ src/main/java/io/github/mbarre/schemacrawler/tool/linter/LinterXmlContent.java
@@ -24,14 +24,19 @@ package io.github.mbarre.schemacrawler.tool.linter;
 
 import io.github.mbarre.schemacrawler.utils.LintUtils;
 import io.github.mbarre.schemacrawler.utils.XmlUtils;
+import org.apache.commons.lang3.RandomUtils;
 import schemacrawler.schema.Column;
 import schemacrawler.schema.Table;
+import schemacrawler.schemacrawler.Config;
 import schemacrawler.schemacrawler.SchemaCrawlerException;
 import schemacrawler.tools.lint.BaseLinter;
 import schemacrawler.tools.lint.LintSeverity;
 
 import java.sql.*;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -41,16 +46,26 @@ import java.util.logging.Logger;
  * @since 1.0.1
  */
 public class LinterXmlContent extends BaseLinter {
-    
+
     private static final Logger LOGGER = Logger.getLogger(LinterXmlContent.class.getName());
-    
+
+    public static final String SAMPLE_SIZE_PARAM = "sampleSize";
+
+    private Integer sampleSize;
+
     /**
      * The lint
      */
     public LinterXmlContent () {
+        sampleSize = 1000;
         setSeverity(LintSeverity.high);
     }
-    
+
+    @Override
+    public void configure(Config config) {
+        sampleSize = config.getIntegerValue(SAMPLE_SIZE_PARAM, 1000);
+    }
+
     /**
      * Get lint description
      * @return lint description
@@ -59,7 +74,7 @@ public class LinterXmlContent extends BaseLinter {
     public String getDescription() {
         return getSummary();
     }
-    
+
     /**
      * Get lint summary
      * @return summary
@@ -68,7 +83,7 @@ public class LinterXmlContent extends BaseLinter {
     public String getSummary() {
         return "should be XML type";
     }
-    
+
     /**
      * The lint that does the job
      * @param table table
@@ -78,31 +93,48 @@ public class LinterXmlContent extends BaseLinter {
     @Override
     protected void lint(final Table table, final Connection connection)
             throws SchemaCrawlerException {
-        
+
+        LOGGER.log(Level.CONFIG, "<"+SAMPLE_SIZE_PARAM+"> parameter set to {0}%", sampleSize);
+
         try (Statement stmt = connection.createStatement()){
-            
             String sql;
             String columnName;
             String tableName = table.getName().replaceAll("\"", "");
             List<Column> columns = getColumns(table);
+
+            Long totalRows = 0L;
+            try(ResultSet rs0 = stmt.executeQuery("select count(*) from \"" + tableName +"\"")){
+                rs0.next();
+                totalRows = rs0.getLong(1);
+            }catch (SQLException ex) {
+                LOGGER.severe(ex.getMessage());
+                throw new SchemaCrawlerException(ex.getMessage(), ex);
+            }
+            LOGGER.log(Level.INFO, "totalrows="+totalRows);
+            Set<Long> sampleIndexes = generateSample(sampleSize, totalRows);
+            LOGGER.log(Level.INFO, "sampleIndexes="+sampleIndexes);
             for (Column column : columns) {
-                
+
                 if(LintUtils.isSqlTypeTextBased(column.getColumnDataType().getJavaSqlType().getJavaSqlType())){
-                    
+
                     columnName = column.getName().replaceAll("\"", "");
-                    
+
                     sql = "select \"" + columnName + "\" from \"" + tableName +"\" where \"" + columnName + "\" like '<%>'" ;
                     LOGGER.log(Level.CONFIG, "SQL : {0}", sql);
                     LOGGER.log(Level.INFO, "Checking {0}...", column.getFullName());
-                    
+
                     try(ResultSet rs = stmt.executeQuery(sql)){
                         boolean found = false;
+                        Long i = 0L;
                         while (rs.next() && !found) {
-                            String data = rs.getString(column.getName());
-                            if(XmlUtils.isXmlContent(data)){
-                                addLint(table, getDescription(), column.getFullName());
-                                found = true;
+                            if(sampleIndexes.contains(i)) {
+                                String data = rs.getString(column.getName());
+                                if (XmlUtils.isXmlContent(data)) {
+                                    addLint(table, getDescription(), column.getFullName());
+                                    found = true;
+                                }
                             }
+                            i++;
                         }
                     }catch (SQLException ex) {
                         LOGGER.severe(ex.getMessage());
@@ -113,10 +145,27 @@ public class LinterXmlContent extends BaseLinter {
                     //TODO voir comment gérer ce cas
                 }
             }
-            
+
         }catch (SQLException ex) {
             LOGGER.severe(ex.getMessage());
             throw new SchemaCrawlerException(ex.getMessage(), ex);
         }
     }
+
+
+
+    private Set<Long> generateSample(int sampleSize, Long totalRows){
+        Set<Long> sampleIndex = new HashSet<>();
+        if(totalRows < sampleSize)
+            sampleSize = totalRows.intValue();
+        while (sampleIndex.size() < sampleSize){
+            Long value = RandomUtils.nextLong(0,totalRows);
+            LOGGER.log(Level.INFO, "value="+value);
+            if(value.compareTo(totalRows) <= 0 && !sampleIndex.contains(value)) {
+                sampleIndex.add(value);
+            }
+        }
+        return sampleIndex;
+    }
+
 }
